6f069b4f4a85c3f73ebae934f54c98b142197a5c
huusangcv <huusangcv@gmail.com>
2025-02-17 21:39:00 +0700

    fix: test router categories with vercel

--- src/router/certificates.ts
+++ src/router/certificates.ts
@@ -12,13 +12,13 @@ import { uploadCertificateImage } from '../utils';
 import { isAdmin, isAuthenticated } from '../app/middlewares';
 
 export default (router: express.Router) => {
+  router.get('/', getAllCertificates);
   router.get('/certificates/search', searchCertificates);
   router.get('/certificates/:id', detailCertificate);
-  router.get('/certificates', getAllCertificates);
 
   //Router for admin
-  router.post('/certificates/uploads/photo',uploadCertificateImage.single('file'),  uploadImage);
-  router.post('/certificates',  uploadCertificateImage.single('image'), createCertificate);
-  router.patch('/certificates/:id',  uploadCertificateImage.single('image'), updateCertificate);
-  router.delete('/certificates/:id',  deleteCertificate);
+  router.post('/certificates/uploads/photo', uploadCertificateImage.single('file'), uploadImage);
+  router.post('/certificates', uploadCertificateImage.single('image'), createCertificate);
+  router.patch('/certificates/:id', uploadCertificateImage.single('image'), updateCertificate);
+  router.delete('/certificates/:id', deleteCertificate);
 };
