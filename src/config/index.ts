61e16dc4caa3110b3067b435acba8947ccaa13dc
huusangcv <huusangcv@gmail.com>
2025-03-08 00:44:07 +0700

    fix: rerender 3 -> update port and url 2

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,11 +3,11 @@ const mongoUrl =
   process.env.MONGODB_CONNECT_URL ||
   'mongodb+srv://huusangcv:HN9j5TFizPCyNP6c@myphammyhanh.k4yz4.mongodb.net/?retryWrites=true&w=majority&appName=myphammyhanh';
 
+console.log('mongoUrl', mongoUrl);
 const connect = async () => {
   try {
     await mongoose.connect(mongoUrl, {
-      serverSelectionTimeoutMS: 10000, // Thay đổi thời gian timeout nếu cần
-      socketTimeoutMS: 45000, // Thay đổi timeout cho socket
+      serverSelectionTimeoutMS: 10000,
     });
     console.log('Connect successfully');
   } catch (error) {
